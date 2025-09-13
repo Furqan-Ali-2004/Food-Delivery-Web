ff581abb70fb40fea07ee68a2c63f11238975e35
Furqan-Ali-2004 <furqangraphy2004@gmail.com>
2025-09-13 14:50:00 +0500

    Add Search Feature

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import { MdFastfood } from "react-icons/md";
 import { IoSearch } from "react-icons/io5";
 import { FaCartShopping } from "react-icons/fa6";
+import { dataContext } from "../context/UserContext.jsx";
+import { food_items } from "../food.js";
 
 function Nav() {
+  let { input, setInput, cate, setCate } = useContext(dataContext);
+
+  useEffect(() => {
+    let newList = food_items.filter((item) => {
+      return item.food_name.toLowerCase().includes(input.toLowerCase());
+    });
+    setCate(newList);
+  }, [input]);
   return (
-    <div className="w-full h-[100px]  flex justify-between items-center px-8">
+    <div className="w-full h-[100px] flex justify-between items-center px-8">
       <div className="w-[60px] h-[60px] bg-zinc-800  flex items-center justify-center rounded-full shadow-orange-500/15 shadow-lg hover:shadow-orange-500/40 duration-200 cursor-pointer">
         <MdFastfood className="w-[28px] h-[28px] text-orange-500" />
       </div>
-      <form className="w-[45%] h-[60px] bg-zinc-800 rounded-full flex items-center px-5 gap-5 shadow-orange-500/15 shadow-lg focus-within:shadow-orange-500/30 duration-150 md:w-[70%]">
+      <form
+        className="w-[45%] h-[60px] bg-zinc-800 rounded-full flex items-center px-5 gap-5 shadow-orange-500/15 shadow-lg focus-within:shadow-orange-500/30 duration-150 md:w-[70%]"
+        onSubmit={(e) => e.preventDefault()}
+      >
         <IoSearch className="h-[20px] w-[20px] text-orange-500" />
         <input
           type="text"
           placeholder="Search Items..."
           className="w-[100%] h-[100%] bg-transparent outline-none text-white placeholder:text-zinc-500 text-[16px] md:text-[20px]"
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
         />
       </form>
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
 import Nav from "../components/Nav.jsx";
 import Categories from "../Category.jsx";
 import Card from "../components/Card.jsx";
 import { food_items } from "../food.js";
+import { dataContext } from "../context/UserContext.jsx";
 
 function Home() {
-  let [cate, setCate] = useState(food_items);
+  let { cate, setCate, input } = useContext(dataContext);
 
   function filter(category) {
     if (category === "All") {
@@ -21,21 +22,22 @@ function Home() {
   return (
     <div className="bg-zinc-900 min-h-screen">
       <Nav />
-
-      <div className="flex flex-wrap justify-center items-center gap-5 w-[100%] h-[100%] mt-6">
-        {Categories.map((item) => {
-          return (
-            <div
-              key={item.id}
-              className="w-[130px] h-[130px] flex flex-col justify-center items-center bg-zinc-800 rounded-full color text-zinc-300 font-semibold gap-2 p-5 shadow-orange-500/15 shadow-lg hover:shadow-orange-500/40 hover:scale-105 duration-150 cursor-pointer"
-              onClick={() => filter(item.name)}
-            >
-              {item.icon}
-              {item.name}
-            </div>
-          );
-        })}
-      </div>
+      {!input ? (
+        <div className="flex flex-wrap justify-center items-center gap-5 w-[100%] h-[100%] mt-6">
+          {Categories.map((item) => {
+            return (
+              <div
+                key={item.id}
+                className="w-[130px] h-[130px] flex flex-col justify-center items-center bg-zinc-800 rounded-full color text-zinc-300 font-semibold gap-2 p-5 shadow-orange-500/15 shadow-lg hover:shadow-orange-500/40 hover:scale-105 duration-150 cursor-pointer"
+                onClick={() => filter(item.name)}
+              >
+                {item.icon}
+                {item.name}
+              </div>
+            );
+          })}
+        </div>
+      ) : null}
 
       <div className="w-full flex flex-wrap justify-center items-center gap-7 pt-14 pb-10">
         {cate.map((item) => (
@@ -48,6 +50,10 @@ function Home() {
             type={item.food_type}
           />
         ))}
+      </div>
+
+      <div className="w-[40vw] h-[650px] fixed top-10 right-5 bg-white rounded-3xl">
+        <header></header>
       </div>
     </div>
   );
